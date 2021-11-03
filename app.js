e01326168b91aa363464dcedcb6db7d81278d13d
esiale <blazejp0@gmail.com>
2021-11-03 20:55:48 +0100

    Add footer, header and basic stylic

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => res.render('index'));
+app.get('/', (req, res) => res.render('index', { title: 'homepage' }));
 
 app.use((req, res, next) => {
   next(createError(404));
